391402851b0e5b09eac6db672805d92a4061d33d
HaQadosch Berraka <haqadosch@pm.me>
2019-09-29 17:57:25 +0100

    Rendering React from a test

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,5 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 describe('Appointment', () => {
   it('should render the customer first name', () => {
+    const customer = { firstName: 'Ashley' }
+    const component = <Appointment customer={ customer } />
+    const container = document.createElement('div')
+    document.appendChild(container)
+
+    ReactDOM.render(component, container)
+
     expect(document.body.textContent).toMatch('Ashley')
   });
 });
