b0455cd60d84ac4da549fe79455ceb606f89f696
HaQadosch Berraka <haqadosch@pm.me>
2019-09-29 18:09:23 +0100

    Make it pass

---
+++ appointments/src/Appointment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Appointment = () => {
+  return (
+    <div>
+      Ashley
+    </div>
+  )
+}
--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Appointment } from '../src/Appointment'
+
 describe('Appointment', () => {
   it('should render the customer first name', () => {
     const customer = { firstName: 'Ashley' }
     const component = <Appointment customer={ customer } />
     const container = document.createElement('div')
-    document.appendChild(container)
+    document.body.appendChild(container)
 
     ReactDOM.render(component, container)
 
     expect(document.body.textContent).toMatch('Ashley')
+  });
+
+  it('should render another customer first name', () => {
+    const customer = { firstName: 'Jordan' }
+    const component = <Appointment customer={ customer } />
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+
+    ReactDOM.render(component, container)
+
+    expect(document.body.textContent).toMatch('Jordan')
   });
 });
