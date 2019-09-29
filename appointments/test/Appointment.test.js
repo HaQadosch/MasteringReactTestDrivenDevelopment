f74e8d7b950d7cad0d9ae960fd7294a13aebfde1
HaQadosch Berraka <haqadosch@pm.me>
2019-09-29 18:32:03 +0100

    ch01 lvl1 sec13, refactoring your work

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -3,21 +3,28 @@ import ReactDOM from 'react-dom'
 
 import { Appointment } from '../src/Appointment'
 
-describe('Appointment', () => {
-  it('should render the customer first name', () => {
-    const customer = { firstName: 'Ashley' }
-    const container = document.createElement('div')
 
-    ReactDOM.render(<Appointment customer={ customer } />, container)
+let container
+beforeEach(() => {
+  container = document.createElement('div')
+})
+
+describe('Appointment', () => {
+  const render = component => ReactDOM.render(component, container)
+
+  let customer
+  it('should render the customer first name', () => {
+    customer = { firstName: 'Ashley' }
+
+    render(<Appointment customer={ customer } />)
 
     expect(container.textContent).toMatch('Ashley')
   });
 
   it('should render another customer first name', () => {
-    const customer = { firstName: 'Jordan' }
-    const container = document.createElement('div')
+    customer = { firstName: 'Jordan' }
 
-    ReactDOM.render(<Appointment customer={ customer } />, container)
+    render(<Appointment customer={ customer } />)
 
     expect(container.textContent).toMatch('Jordan')
   });
