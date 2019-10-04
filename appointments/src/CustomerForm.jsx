6f64e2904d74b473d3e49e54f993c7f0a0e28a05
haqadosch <haqadosch@pm.me>
2019-10-04 14:14:48 +0100

    Saving the customer information

--- appointments/src/CustomerForm.jsx
+++ appointments/src/CustomerForm.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import produce from 'immer'
 
-export const CustomerForm = ({ firstName }) => (
-  <form action='' id='customer'>
-    <label htmlFor='firstName' >First Name</label>
-    <input type='text' id='firstName' name='firstName' readOnly value={firstName} />
-  </form>
-)
+export const CustomerForm = ({ firstName, onSubmit }) => {
+  const [customer, setCustomer] = useState({ firstName })
+
+  const handleInputOnChange = ({ target: { value } }) => {
+    setCustomer(customer => produce(customer, draft => { draft.firstName = value }))
+  }
+
+  return (
+    <form action='' id='customer' onSubmit={() => onSubmit(customer)}>
+      <label htmlFor='firstName' >First Name</label>
+      <input type='text' id='firstName' name='firstName' onChange={handleInputOnChange} value={firstName} />
+    </form>
+  )
+}
--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactTestUtils from 'react-dom/test-utils'
 import { createContainer } from './domManipulators'
 import { CustomerForm } from '../src/CustomerForm'
 
@@ -45,5 +46,17 @@ describe('CustomerForm ', () => {
   it('should assign an id that matches the label id to the first name field', () => {
     render(<CustomerForm />)
     expect(firstNameField().id).toEqual('firstName')
+  })
+
+  it('should save existing first name when submitted', async () => {
+    expect.hasAssertions()
+    render(<CustomerForm
+      firstName='Ashley'
+      onSubmit={({ firstName }) => expect(firstName).toEqual('Jamie')}
+    />)
+    await ReactTestUtils.Simulate.change(firstNameField(), {
+      target: { value: 'Jamie' }
+    })
+    await ReactTestUtils.Simulate.submit(form('customer'))
   })
 })
